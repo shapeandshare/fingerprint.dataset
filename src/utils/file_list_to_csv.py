88e768128bd4f3eab4bb582fcda2e9c2498f7c16
Joshua Burt <josh.burt@ephesoft.com>
2022-03-05 10:35:21 -0800

    some output

--- src/utils/file_list_to_csv.py
+++ src/utils/file_list_to_csv.py
@@ -3,12 +3,15 @@ import json
 from pathlib import Path
 from typing import List
 
+from tqdm import tqdm
+
 from ..contracts.dtos.file_record import FileRecord
 from ..contracts.source_type import SourceType
 
 # Load data set
 data_set: List[FileRecord] = []
-for child in (Path(".") / "hashes").glob("**/*.json"):
+print("loading data set")
+for child in tqdm((Path(".") / "hashes").glob("**/*.json")):
     if child.is_file():
         with open(child.resolve().as_posix(), mode="r", encoding="utf-8") as file:
             data_set.append(FileRecord(**json.load(file)))
@@ -20,12 +23,14 @@ tabular_representation: List = []
 headers: List = ["hash.name", "path", "hash.data"]
 
 tabular_representation.append(headers)
-for record in data_set:
+print("converting to csv")
+for record in tqdm(data_set):
     name_hash = [hash for hash in record.hash if hash.source == SourceType.NAME][0]
     data_hash = [hash for hash in record.hash if hash.source == SourceType.DATA][0]
     row: List = [name_hash.value, record.path, data_hash.value]
     tabular_representation.append(row)
 
+print("saving")
 with open((Path(".") / "file_list.csv").resolve().as_posix(), mode="w", encoding="utf-8", newline="") as file:
     writer = csv.writer(file)
     writer.writerows(tabular_representation)
