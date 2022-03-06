a46790d3157b8eb35b2e9892c24dc34e0aa25313
Joshua Burt <josh.burt@ephesoft.com>
2022-03-05 17:45:02 -0800

    to functions

--- src/utils/file_list_to_csv.py
+++ src/utils/file_list_to_csv.py
@@ -8,17 +8,23 @@ from tqdm import tqdm
 from ..contracts.dtos.file_record import FileRecord
 from ..contracts.source_type import SourceType
 
-# Create headers
-headers: List = ["path", "hash.data", "size", "modified"]
 
-print("Building CSV")
-with open((Path(".") / "file_list.csv").resolve().as_posix(), mode="w", encoding="utf-8", newline="") as file:
-    writer = csv.writer(file)
-    writer.writerow(headers)
+def build_csv():
+    # Create headers
+    headers: List = ["path", "hash.data", "size", "modified"]
 
-    for child in tqdm((Path(".") / "hashes").glob("**/*.json")):
-        if child.is_file():
-            with open(child.resolve().as_posix(), mode="r", encoding="utf-8") as file:
-                record: FileRecord = FileRecord(**json.load(file))
-                data_hash = [hash for hash in record.hash if hash.source == SourceType.DATA][0]
-                writer.writerow([child.resolve().as_posix(), data_hash.value, record.size, record.modified])
+    print("Building CSV")
+    with open((Path(".") / "file_list.csv").resolve().as_posix(), mode="w", encoding="utf-8", newline="") as file:
+        writer = csv.writer(file)
+        writer.writerow(headers)
+
+        for child in tqdm((Path(".") / "hashes").glob("**/*.json")):
+            if child.is_file():
+                with open(child.resolve().as_posix(), mode="r", encoding="utf-8") as file:
+                    record: FileRecord = FileRecord(**json.load(file))
+                    data_hash = [hash for hash in record.hash if hash.source == SourceType.DATA][0]
+                    writer.writerow([child.resolve().as_posix(), data_hash.value, record.size, record.modified])
+
+
+if __name__ == "__main__":
+    build_csv()
--- src/utils/file_list_to_df.py
+++ src/utils/file_list_to_df.py
@@ -1,5 +1,11 @@
 from pathlib import Path
 import pandas as pd
 
-files_df: pd.DataFrame = pd.read_csv("file_list.csv")
-files_df.to_pickle(path=(Path(".") / "file_list.df.pkl.bz2").resolve().as_posix(), compression={"method": "bz2"})
+
+def export_to_df():
+    files_df: pd.DataFrame = pd.read_csv("file_list.csv")
+    files_df.to_pickle(path=(Path(".") / "file_list.df.pkl.bz2").resolve().as_posix(), compression={"method": "bz2"})
+
+
+if __name__ == "__main__":
+    export_to_df()
