b65fe502b1681c8c6380f8289f9d197e13f87bb7
Joshua Burt <josh.burt@ephesoft.com>
2022-03-05 17:29:30 -0800

    updates

--- src/utils/file_list_to_csv.py
+++ src/utils/file_list_to_csv.py
@@ -9,7 +9,7 @@ from ..contracts.dtos.file_record import FileRecord
 from ..contracts.source_type import SourceType
 
 # Create headers
-headers: List = ["hash.name", "path", "hash.data"]
+headers: List = ["path", "hash.data", "size", "modified"]
 
 print("Building CSV")
 with open((Path(".") / "file_list.csv").resolve().as_posix(), mode="w", encoding="utf-8", newline="") as file:
@@ -20,6 +20,5 @@ with open((Path(".") / "file_list.csv").resolve().as_posix(), mode="w", encoding
         if child.is_file():
             with open(child.resolve().as_posix(), mode="r", encoding="utf-8") as file:
                 record: FileRecord = FileRecord(**json.load(file))
-                name_hash = [hash for hash in record.hash if hash.source == SourceType.NAME][0]
                 data_hash = [hash for hash in record.hash if hash.source == SourceType.DATA][0]
-                writer.writerow([name_hash.value, record.path, data_hash.value])
+                writer.writerow([child.resolve().as_posix(), data_hash.value, record.size, record.modified])
