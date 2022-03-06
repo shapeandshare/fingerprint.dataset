948c7f4bf34e3fad4e069aa7e0bc8f88b297da31
Joshua Burt <josh.burt@ephesoft.com>
2022-03-06 11:09:04 -0800

    updates

--- src/contracts/dtos/data_set.py
+++ src/contracts/dtos/data_set.py
@@ -1,6 +1,7 @@
 import csv
 import json
 import logging
+from logging import Logger
 from pathlib import Path
 from typing import Any, List, Optional
 
@@ -24,50 +25,71 @@ class DataSet(BaseModel):
     csv_file: Optional[str]
     pickle_file: Optional[str]
 
+    logger: Optional[Logger]
+
+    class Config:
+        arbitrary_types_allowed = True
+
     def __init__(self, **data: Any):
         super().__init__(**data)
+
+        self.logger = logging.getLogger(__name__)
+        # logging.getLogger().setLevel(logging.INFO)
+        # self.logger.setLevel(logging.INFO)
 
         self.metadata_base = Path(".") / self.name
         self.hash_path = self.metadata_base / "hashes"
 
         self.txt_file = (self.metadata_base / f"{self.name}.txt").resolve().as_posix()
         self.csv_file = (self.metadata_base / f"{self.name}.csv").resolve().as_posix()
-        self.pickle_file = (self.metadata_base / f"{self.name}.df.pkl.bz2").resolve().as_posix()
+        self.pickle_file = (
+            (self.metadata_base / f"{self.name}.df.pkl.bz2").resolve().as_posix()
+        )
 
         self.hash_path.mkdir(exist_ok=True, parents=True)
 
     def build_index(self) -> None:
-        logging.info("Indexing Search Path")
+        self.logger.info("Indexing Search Path")
         with open(file=self.txt_file, mode="w", encoding="utf-8") as file:
             for child in tqdm(self.search_path.glob("**/*")):
                 if child.is_file():
                     file.write(f"{child.resolve().as_posix()}\r\n")
 
     def generate_hash(self) -> None:
-        logging.info("Hashing Files")
+        self.logger.info("Hashing Files")
 
         if not Path(self.txt_file).exists():
             self.build_index()
 
         with open(file=self.txt_file, mode="r", encoding="utf-8") as file:
+            # number_lines = sum(1 for line in open(file))
             for line in tqdm(file):
                 file_path: str = line.rstrip()
                 file_path_hash: str = generate_file_path_hash(file_path=file_path)
                 file_record_path: Path = self.hash_path / f"{file_path_hash}.json"
 
                 if file_record_path.exists():
-                    with open(file_record_path.resolve().as_posix(), mode="r", encoding="utf-8") as file:
+                    with open(
+                        file_record_path.resolve().as_posix(),
+                        mode="r",
+                        encoding="utf-8",
+                    ) as file:
                         file_record: FileRecord = generate_hash(
-                            file_path=file_path, file_record=FileRecord(**json.load(file))
+                            file_path=file_path,
+                            file_record=FileRecord(**json.load(file)),
                         )
                 else:
                     file_record: FileRecord = generate_hash(file_path=file_path)
 
-                with open(file_record_path.resolve().as_posix(), mode="w", encoding="utf-8") as file:
-                    file.write(json.dumps(file_record.dict(exclude_unset=True), indent=4))
+                with open(
+                    file_record_path.resolve().as_posix(), mode="w", encoding="utf-8"
+                ) as file:
+                    file.write(
+                        json.dumps(file_record.dict(exclude_unset=True), indent=4)
+                    )
 
     def generate_csv(self):
-        logging.info("Generating CSV")
+        self.logger.info("Generating CSV")
         # Create headers
         headers: List = ["path", "hash.data", "size", "modified"]
 
@@ -77,13 +99,26 @@ class DataSet(BaseModel):
 
             for child in tqdm(self.hash_path.glob("**/*.json")):
                 if child.is_file():
-                    with open(file=child.resolve().as_posix(), mode="r", encoding="utf-8") as file:
+                    with open(
+                        file=child.resolve().as_posix(), mode="r", encoding="utf-8"
+                    ) as file:
                         record: FileRecord = FileRecord(**json.load(file))
-                        data_hash = [hash for hash in record.hash if hash.source == SourceType.DATA][0]
-                        writer.writerow([child.resolve().as_posix(), data_hash.value, record.size, record.modified])
+                        data_hash = [
+                            hash
+                            for hash in record.hash
+                            if hash.source == SourceType.DATA
+                        ][0]
+                        writer.writerow(
+                            [
+                                child.resolve().as_posix(),
+                                data_hash.value,
+                                record.size,
+                                record.modified,
+                            ]
+                        )
 
     def generate_dataframe(self):
-        logging.info("Generating pickled dataframe")
+        self.logger.info("Generating pickled dataframe")
         if not Path(self.csv_file).exists():
             self.generate_csv()
         files_df: pd.DataFrame = pd.read_csv(self.csv_file)
--- src/process_data_set.py
+++ src/process_data_set.py
@@ -1,6 +1,11 @@
+import logging
+
 from .contracts.dtos.data_set import DataSet
 
 if __name__ == "__main__":
+    # Set log level (from default: warning) to info
+    logging.getLogger().setLevel(logging.INFO)
+
     data_set: DataSet = DataSet(name="dataset", search_path=".")
     data_set.build_index()
     data_set.generate_hash()
