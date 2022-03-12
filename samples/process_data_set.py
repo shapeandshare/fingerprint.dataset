b6036fbd7a48bbe60fef4ffee3c9099634db7bc2
Joshua Burt <joshburt@users.noreply.github.com>
2022-03-12 15:56:21 -0800

    project base (#5)

--- samples/process_data_set.py
+++ samples/process_data_set.py
@@ -5,6 +5,6 @@ from shapeandshare.fingerprint.dataset import DataSet
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
 
-    data_set: DataSet = DataSet(name="dataset", search_path=".")
+    data_set: DataSet = DataSet(name="dataset", search_path="./test", metadata_root="./projects")
     data_set.hash()
     data_set.generate_dataframe()
